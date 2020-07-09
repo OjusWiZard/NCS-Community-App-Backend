41b2eac95eda9e39aceaf49582bb75dc6250354e
OjusWiZard <ojuswimail@gmail.com>
2020-07-09 19:11:18 +0530

    added Project serializers, both secure and insecure

--- Projects/views.py
+++ Projects/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Project
+from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ProjectSerializer
 
-# Create your views here.
+
+@permission_classes([IsAuthenticated])
+class ProjectSerializer(viewsets.ModelViewSet):
+    queryset = ProjectSerializer.objects.all().order_by('started_year')
+    serializer_class = ProjectSerializer
+
+
+class ProjectSerializerForNow(viewsets.ModelViewSet):
+    queryset = ProjectSerializer.objects.all().order_by('started_year')
+    serializer_class = ProjectSerializer
