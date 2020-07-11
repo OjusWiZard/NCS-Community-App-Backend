91f0f807a6cba12736ae610017392633308d328a
OjusWiZard <ojuswimail@gmail.com>
2020-07-12 04:38:10 +0530

    added TechStack Serializers

--- Nibblites/serializers.py
+++ Nibblites/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from Accounts.models import User, Profile, User_links
+from Projects.serializers import TechStackSerializer
 from collections import OrderedDict
 from operator import itemgetter
 
@@ -12,9 +13,10 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     profiles = ProfileSerializer(many=True)
+    techstack = TechStackSerializer()
     class Meta:
         model = User
-        fields = ['nickname','full_name','profile_pic','email','year','designation','club','phone_no','profiles']
+        fields = ['nickname','full_name','profile_pic','email','year','designation','club','phone_no','techstack','profiles']
 
 
 class User_linksSerializer(serializers.ModelSerializer):
--- Projects/serializers.py
+++ Projects/serializers.py
@@ -1,6 +1,42 @@
 from rest_framework import serializers
 from .models import Project
 from Accounts.models import User
+from Nibblites.models import Frontend, Backend, AppTech, Language, TechStack
+
+
+class FrontendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Frontend
+        fields = ['frontend_tech']
+
+
+class BackendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Backend
+        fields = ['backend_tech']
+
+
+class AppTechSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppTech
+        fields = ['app_tech']
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ['language']
+
+
+class TechStackSerializer(serializers.ModelSerializer):
+    frontend_techs = FrontendSerializer(many=True,read_only=True)
+    backend_techs = BackendSerializer(many=True,read_only=True)
+    app_techs = AppTechSerializer(many=True,read_only=True)
+    languages = LanguageSerializer(many=True,read_only=True)
+
+    class Meta:
+        model = TechStack
+        fields = ['languages','frontend_techs','backend_techs','app_techs']
 
 
 class TeamSerializer(serializers.ModelSerializer):
@@ -11,6 +47,7 @@ class TeamSerializer(serializers.ModelSerializer):
 
 class ProjectSerializer(serializers.ModelSerializer):
     team = TeamSerializer(many=True)
+    techstack = TechStackSerializer()
     class Meta:
         model = Project
-        fields = ['project_name','project_description','team','current_status','github','deployed_at','icon','background','started_year','last_modified','scope']
+        fields = ['project_name','project_description','team','techstack','current_status','github','deployed_at','icon','background','started_year','last_modified','scope']
