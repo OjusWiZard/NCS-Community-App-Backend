ba9ee2f0f1b2fb74269b402c0d584ab28aae4095
OjusWiZard <ojuswimail@gmail.com>
2020-07-15 00:55:43 +0530

    Attendance is getting marked!

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from rest_framework import routers
 from .views import markAttendance
 
+routers = routers.DefaultRouter()
+routers.register(r'', markAttendance, basename='attend')
+
 urlpatterns = [
-    path('<int:venue>', markAttendance, name='attend')
+    path('<int:lab_id>/', include(routers.urls)),
 ]
--- Attendance/views.py
+++ Attendance/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-from .models import Venue, Lab, Attendance
+from datetime import datetime
+from .models import Lab, Attendance, Venue
 
-def markAttendance(request,venue):
-    return HttpResponse("Pass")
+
+@permission_classes([IsAuthenticated])
+class markAttendance(viewsets.ViewSet):
+    
+    def create(self,request,lab_id):
+        get_object_or_404(Venue,pk=lab_id)
+        latest_lab = Lab.objects.get(venue=lab_id)
+        current_time = datetime.now()
+
+        mark = Attendance.objects.create(attendee=request.user,lab=latest_lab,time_entered=current_time)
+        confirmation = AttendanceSerializer(mark)
+        return HttpResponse()
