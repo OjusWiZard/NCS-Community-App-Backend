a2e14d7ee9eeafbd11039589d4421aa09b366d77
OjusWiZard <ojuswimail@gmail.com>
2020-07-15 04:09:59 +0530

    handled all the use cases of attendance marking

--- Attendance/views.py
+++ Attendance/views.py
@@ -12,9 +12,15 @@ class markAttendance(viewsets.ViewSet):
     
     def create(self,request,lab_id):
         get_object_or_404(Venue,pk=lab_id)
-        latest_lab = Lab.objects.get(venue=lab_id)
+
+        if not Lab.objects.filter(venue=lab_id).last():
+            return HttpResponse(status=404)
+
+        latest_lab = Lab.objects.filter(venue=lab_id).last()
         current_time = datetime.now()
 
-        mark = Attendance.objects.create(attendee=request.user,lab=latest_lab,time_entered=current_time)
-        confirmation = AttendanceSerializer(mark)
-        return HttpResponse()
+        if Attendance.objects.filter(attendee=request.user,lab=latest_lab):
+            return HttpResponse(status=208)
+
+        Attendance.objects.create(attendee=request.user,lab=latest_lab,time_entered=current_time)
+        return HttpResponse(status=201)
