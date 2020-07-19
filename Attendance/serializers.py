667e4677642cf0dd4f4f688dfd611dce4279a0bd
OjusWiZard <ojuswimail@gmail.com>
2020-07-20 05:08:57 +0530

    customized date and time of schedule

--- Attendance/serializers.py
+++ Attendance/serializers.py
@@ -13,6 +13,15 @@ class ScheduleSerializer(serializers.ModelSerializer):
     attendees = TeamSerializer(many=True)
     organizer = TeamSerializer()
     venue = VenueSerializer()
+    start_date = serializers.SerializerMethodField('custom_date')
+    start_time = serializers.SerializerMethodField('custom_time')
+
+    def custom_date(self, lab):
+        return lab.start_datetime.strftime('%a, %d %b, %Y')
+    
+    def custom_time(self, lab):
+        return lab.start_datetime.strftime('%I:%M %p')
+
     class Meta:
         model = Lab
-        fields = ['start_datetime','attendance_offset','duration','topic','additional_info','venue','organizer','attendees']
+        fields = ['start_date','start_time','attendance_offset','duration','topic','additional_info','venue','organizer','attendees']
