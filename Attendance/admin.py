4bfa90ee348c93bcce3e6ea5bdb52595ee513358
OjusWiZard <ojuswimail@gmail.com>
2020-07-23 04:33:45 +0530

    Added action to Announce from the admin panel

--- Attendance/admin.py
+++ Attendance/admin.py
@@ -1,18 +1,32 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.auth.models import Group
+from django.db.models import Exists, Subquery, Q
 from fcm_django.models import FCMDevice
 from .models import Announcement, Lab, Attendance, Venue
+from Accounts.models import User
 
-def Announce(Announcement,request,queryset):
+def Announce(AnnouncementAdmin,request,queryset):
 
-    announcements = list(queryset)
-    notifyingGroups = [ group for announcement in announcements for group in list( announcement.notify_groups.all() ) ] # Very UGLY way to get all groups from all announcements
-    notifyingUsers = [ user for group in notifyingGroups for user in list( group.user_set.all() ) ] # Very UGLY way to get all users from all groups
-    notifyingUsers += [ user for announcement in announcements for user in list( announcement.notify_users.all() ) ] # Very UGLY way to append all users from all announcements
-    notifyingDevices = [ device for user in notifyingUsers for device in list( user.fcmdevice_set.all() ) ] # Very UGLY way to get all devices of all users
+    for announcement in queryset:
+        
+        notifyingGroups = Group.objects.filter( id=Exists( announcement.notify_groups.all() ) )
+        notifyingUsers = User.objects.filter( Q( groups__in=notifyingGroups ) | Q( id__in=announcement.notify_users.get_queryset() ) )
+        notifyingDevices = FCMDevice.objects.filter( user__in=notifyingUsers )
 
-    for device in notifyingDevices:
-        device.send_message("Title", "Message")
-        print("sending to "+str(device))
+        response = notifyingDevices.send_message( title=announcement.title, body=announcement.message )
+        if response:
+            successes = response['success']
+            failures = response['failure']
+            result = 'Announcement Title: ' + announcement.title + ', Successfully Sent: ' + str(response['success']) + ', Failed to Send: ' + str(response['failure'])
+
+            if not response['failure']:
+                code = messages.SUCCESS
+            elif response['success']>0 and response['failure']>0:
+                code = messages.WARNING
+            else:
+                code = messages.ERROR
+
+            AnnouncementAdmin.message_user(request, result, code )
 
 class AnnouncementAdmin(admin.ModelAdmin):
     actions = [Announce]
