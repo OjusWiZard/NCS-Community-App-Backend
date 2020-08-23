6b74aad1a337b096560e6577a848922df918c919
OjusWiZard <ojuswimail@gmail.com>
2020-08-23 15:24:07 +0530

    Phone number is shown to only Nibblites

--- Nibblites/serializers.py
+++ Nibblites/serializers.py
@@ -18,6 +18,12 @@ class ProfileSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     profiles = ProfileSerializer(many=True)
     techstack = TechStackSerializer()
+    phone_no = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['nickname','full_name','profile_pic','email','year','designation','club','phone_no','techstack','profiles']
+        fields = ['nickname', 'full_name', 'profile_pic', 'email', 'year', 'designation', 'club', 'phone_no', 'techstack', 'profiles']
+    def get_phone_no(self, objects):
+        if self.context['request'].user in User.objects.filter(groups__name='Nibblite'):
+            return objects.phone_no
+        else:
+            return None
